a9c958d7c17ad9ff5a02c6eb3af2725bc4806bee
alanbuttars <alanbuttars@gmail.com>
2017-06-03 21:46:18 -0500

    Auto-generated commit

--- src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: "<my-header></my-header><my-nav></my-nav><router-outlet></router-outlet>",
-})
-export class AppComponent  { name = 'Angular'; }
