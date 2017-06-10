59b7ea8cf2cb29997fbd2e196b1a83f173adfb9f
alanbuttars <alanbuttars@gmail.com>
2017-06-10 18:48:04 -0500

    Adding styling and logo images

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  providers: [Location]
 })
 export class HeaderComponent {
-  title = 'Header';
 }
